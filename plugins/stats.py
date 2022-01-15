bed4ff0fa9efdf4ae76e929daf52a9fd68a8d6c0
5samagi <96784101+5samagi@users.noreply.github.com>
2022-01-15 19:36:16 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -45,7 +45,7 @@ async def shell(client, message):
                 stats += "\n\n💎 " + Config.CHANNEL_OR_CONTACT
             await message.reply_text(stats, reply_to_message_id = message.message_id)
         except:
-            await message.reply_text("🇬🇧 Maybe your shell message was empty.",
+            await message.reply_text("Maybe your shell message was empty.",
                     reply_to_message_id = message.message_id)
             return
             
